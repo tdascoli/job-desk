a14011e2cf36e28ec9f4989c92d861e1e74f1d5f
Patrick Ellenberger <p.ellenberger@outlook.com>
2015-10-07 16:25:09 +0200

    Add educations printing

--- src/main/scripts/app/educations/educations.directive.js
+++ src/main/scripts/app/educations/educations.directive.js
@@ -4,7 +4,7 @@
 
   var module = angular.module('job-desk');
 
-  module.directive('educationDetail', ['$translate', function($translate){
+  module.directive('educationDetail', ['$translate', '$mdDialog', '$timeout', function($translate, $mdDialog, $timeout){
     return {
       priority: 10,
       restrict: 'A',
@@ -29,6 +29,37 @@
             return 'other';
           }
           return lang;
+        };
+
+        scope.showPrintDialog = function() {
+          $mdDialog.show({
+            parent: angular.element(document.body),
+            templateUrl: 'views/template/education-print.html',
+            locals: {
+              educationDetail: scope.educationDetail,
+              getMultiLanguageText: scope.getMultiLanguageText,
+              checkLanguage: scope.checkLanguage
+            },
+            escapeToClose: false,
+            onComplete: function() {
+              window.print();
+              $timeout(function () {
+                $mdDialog.hide();
+              }, 3000);
+            },
+            controller: function PrintDialogController($scope, educationDetail, getMultiLanguageText, checkLanguage) {
+              $scope.educationDetail = educationDetail;
+              $scope.getMultiLanguageText = getMultiLanguageText;
+              $scope.checkLanguage = checkLanguage;
+              $scope.cancel = function() {
+                $mdDialog.cancel();
+              };
+              $scope.formatText = function(text) {
+                text = text.replace(/(<|&lt;)br\s*\/*(>|&gt;)/g,' ');
+                return text;
+              };
+            }
+          });
         };
       }
     };
