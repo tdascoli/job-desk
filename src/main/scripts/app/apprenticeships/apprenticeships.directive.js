8d9f6b5f4b423e807a10a0b426021853fee1fbd6
Patrick Ellenberger <p.ellenberger@outlook.com>
2015-10-08 11:38:53 +0200

    Add apprenticeships printing + improve educations printing

--- src/main/scripts/app/apprenticeships/apprenticeships.directive.js
+++ src/main/scripts/app/apprenticeships/apprenticeships.directive.js
@@ -4,7 +4,7 @@
 
   var module = angular.module('job-desk');
 
-  module.directive('apprenticeshipDetail', ['$translate', function($translate){
+  module.directive('apprenticeshipDetail', ['$translate', '$mdDialog', '$timeout', function($translate, $mdDialog, $timeout){
     return {
       priority: 10,
       restrict: 'A',
@@ -16,6 +16,35 @@
 
         scope.getMultiLanguageText=function(text){
           return text[$translate.use()];
+        };
+
+        scope.showPrintDialog = function() {
+          $mdDialog.show({
+            parent: angular.element(document.body),
+            templateUrl: 'views/template/apprenticeship-print.html',
+            locals: {
+              apprenticeshipDetail: scope.apprenticeshipDetail,
+              getMultiLanguageText: scope.getMultiLanguageText
+            },
+            escapeToClose: false,
+            onComplete: function() {
+              window.print();
+              $timeout(function () {
+                $mdDialog.hide();
+              }, 3000);
+            },
+            controller: function PrintDialogController($scope, apprenticeshipDetail, getMultiLanguageText) {
+              $scope.apprenticeshipDetail = apprenticeshipDetail;
+              $scope.getMultiLanguageText = getMultiLanguageText;
+              $scope.cancel = function() {
+                $mdDialog.cancel();
+              };
+              $scope.formatText = function(text) {
+                text = text.replace(/(<|&lt;)br\s*\/*(>|&gt;)/g,' ');
+                return text;
+              };
+            }
+          });
         };
       }
     };
