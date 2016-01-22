328904691b047e7b57c3fc98b24552aa7d022a13
Thomas D'Ascoli <thomas@dasco.li>
2016-01-22 14:15:30 +0100

    update bower.json

--- src/main/scripts/app/app.js
+++ src/main/scripts/app/app.js
@@ -143,12 +143,7 @@
     }
 
     $rootScope.back = function () {
-      if ($state.$current.url.source === '/' || $state.$current.url.source === '/jobs' || $state.$current.url.source === '/apprenticeships' || $state.$current.url.source === '/educations') {
-        //$state.go('localInfo');
-      }
-      else {
-        $state.go($rootScope.searchType);
-      }
+      $state.go($rootScope.searchType);
     };
 
     // detection of user inactivity
