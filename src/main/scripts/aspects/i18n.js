2e1e7734c0b6eaaf522e028f6dfa25f3bcd89d3c
Thomas D'Ascoli <thomas@dasco.li>
2016-05-10 14:55:17 +0200

    fixed umlaute problem in angular translate: http://angular-translate.github.io/docs/#/guide/19_security

--- src/main/scripts/aspects/i18n.js
+++ src/main/scripts/aspects/i18n.js
@@ -20,7 +20,7 @@
     });
     $translateProvider.useCookieStorage();
     $translateProvider.determinePreferredLanguage();
-    $translateProvider.useSanitizeValueStrategy('sanitize');
+    $translateProvider.useSanitizeValueStrategy('escaped');
     tmhDynamicLocaleProvider.localeLocationPattern('bower_components/angular-i18n/angular-locale_{{locale}}.js');
     tmhDynamicLocaleProvider.useCookieStorage('NG_TRANSLATE_LANG_KEY');
   });
